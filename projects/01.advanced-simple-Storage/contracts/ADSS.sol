901041e7f63d84d7643245a59741c14b6d0838ff
batscar <bw229032.bat@gmail.com>
2026-03-31 01:15:09 +0530

    ADSS

--- projects/01.advanced-simple-Storage/contracts/ADSS.sol
+++ projects/01.advanced-simple-Storage/contracts/ADSS.sol
@@ -1,7 +1,77 @@
 //SPDX-License-Identifier:MIT
- pragma solidity ^0.8.0;
 
- contract simpleStorage{
+pragma solidity ^0.8.21;
 
-    
- }
+contract AdvancedSimpleStorage {
+    //state variable
+    uint256 public favoriteNumber;
+    string public name;
+    bool public isActive;
+
+    // immutable variable
+    address public immutable owner;
+
+    event NumberUpdated(uint256 oldNumber, uint256 newNumber , address changedBy);
+    event NameUpdated(string oldName, string newName);
+    event ActiveStatusChanged(bool newStatus, address changedBy);
+
+  
+    error OnlyOwnerCanCall();
+
+    constructor() {
+        owner = msg.sender;
+        name = "rodger";
+        isActive = true;
+
+    }
+
+    function setFavoriteNumber(uint256 _newNumber) public {
+        uint256  oldNumber = favoriteNumber;
+        favoriteNumber = _newNumber ;
+
+        emit NumberUpdated(oldNumber, _newNumber,  msg.sender );
+
+    }
+
+    function setName(string memory _newName)  public {
+        if (msg.sender != owner) {
+            revert OnlyOwnerCanCall();
+        }
+
+        string memory  oldName = name;
+         name = _newName;
+
+        emit NameUpdated(oldName, _newName);
+
+    }
+
+      function toggleActive() public onlyOwner {
+        isActive = !isActive;
+        emit ActiveStatusChanged(isActive, msg.sender);
+    }
+
+    function getFavoriteNumber() public view returns (uint256){
+
+        return favoriteNumber;
+    }
+
+    modifier onlyOwner() {
+     if (msg.sender != owner) {
+        revert OnlyOwnerCanCall();
+     }
+     _;
+
+    }
+
+    function resetAll() public onlyOwner {
+        uint256 oldNumber = favoriteNumber;
+        string memory oldName = name;
+
+        favoriteNumber = 0;
+        name = "Default";
+        emit NameUpdated(oldName, "Default");
+        emit NumberUpdated(oldNumber, 0, msg.sender);
+
+    }
+
+}
